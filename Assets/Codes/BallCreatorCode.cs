4481c39c219914f18aaace2dea5a5774625a6dc0
koray <koraysarioglu@yahoo.com>
2020-04-25 18:41:24 +0300

    collision detection added for boxes

---
+++ Assets/Codes/BallCreatorCode.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCreatorCode : MonoBehaviour
+{
+    public GameObject loc;
+
+    public GameObject[] spherePrefab;
+
+    private Transform locT;
+
+
+    private int seconds;
+    private float timer;
+    private float mseconds;
+
+    List<GameObject> mylist;
+    public int idx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        locT = loc.GetComponent<Transform>();
+        mylist = new List<GameObject>();
+        mseconds = 5;
+        StartCoroutine(Spawn());
+
+        InvokeRepeating("timedivider", 5, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    IEnumerator Spawn()
+    {
+        while(true)
+        {
+           
+            int x = Random.Range(0, 299);
+            float randX = Random.Range(-3f, 3f);
+
+
+            Instantiate(spherePrefab[x / 100], new Vector3(randX, locT.position.y, locT.position.z), Quaternion.identity);
+            
+            Debug.Log(mylist.Count + ". block created.");
+            yield return new WaitForSeconds(mseconds);
+        }
+        
+    }
+
+    void timedivider()
+    {
+        mseconds /= 2;
+    }
+
+}
---
+++ Assets/Codes/SquareCode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareCode : MonoBehaviour
+{
+    private int x = 0;
+    private int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = Random.Range(1, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0)
+            Destroy(gameObject, 0.1f);
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ball")
+        {
+            --health;
+        }
+    }
+
+}
