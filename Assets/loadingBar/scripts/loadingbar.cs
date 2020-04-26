523e379310ccc557c252506706038ca515810eb8
Hakan <hksarioglu@gmail.com>
2020-04-27 00:23:01 +0300

    leading bar added. ball creation is connected with loading bar

---
+++ Assets/loadingBar/scripts/loadingbar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadingbar : MonoBehaviour {
+
+    private RectTransform rectComponent;
+    private Image imageComp;
+    public float speed = 0.0f;
+
+    public bool filled = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        rectComponent = GetComponent<RectTransform>();
+        imageComp = rectComponent.GetComponent<Image>();
+        imageComp.fillAmount = 0.0f;
+    }
+
+    void Update()
+    {
+        if (imageComp.fillAmount != 1f)
+        {
+            if(Input.GetKey(KeyCode.Space))
+            {
+                Debug.Log("Space Pressed.");
+                imageComp.fillAmount = imageComp.fillAmount + Time.deltaTime * speed * 2;
+            } else
+            {
+                imageComp.fillAmount = imageComp.fillAmount + Time.deltaTime * speed;
+            }          
+            filled = false;
+        }
+        else
+        {
+            imageComp.fillAmount = 0.0f;
+            filled = true;
+        }
+
+    }
+}
